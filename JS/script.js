bab7d26910b7831527639edd54041d72c9f97253
BrambillaMarco <marco.brambilla2000@gmail.com>
2023-03-29 18:11:09 +0200

    aggiunti i messaggi di loading per il searchbook e altri controlli

--- JS/script.js
+++ JS/script.js
@@ -4,15 +4,33 @@ function searchBooks(category) {
   const bookList = document.getElementById("books");
   bookList.innerHTML = "";
 
-
+ // Creazione del div per il caricamento
+  const loadingDiv = document.createElement("div");
+  loadingDiv.id = "loading";
+  loadingDiv.innerHTML = "Loading...";
+  loadingDiv.style.display = "block";
+  document.body.appendChild(loadingDiv);
   fetch(url)
     .then(response => response.json())
     .then(data => {
       const books = data.works;
-      
+     
       const bookList = document.getElementById("books");
       bookList.innerHTML = "";
 
+
+      //tolgo il messaggio di caricamento
+      const loadingDiv = document.getElementById("loading");
+      loadingDiv.style.display = "none";
+
+       // controlla se ci sono libri nella categoria
+       if (!books || books.length === 0) {
+         bookList.innerHTML = `<div class="text-center">Nessun libro trovato.</div>`;
+         
+         loadingDiv.style.display = "none";
+        return;
+      }
+      
       books.forEach(book => {
         const li = document.createElement("li");
         li.innerHTML = `<strong>TITLE:</strong> ${book.title} <br> <strong>AUTHOR:</strong> ${book.authors.map(author => author.name).join(", ")}`;
@@ -21,7 +39,13 @@ function searchBooks(category) {
         bookList.appendChild(li);
       });
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+      console.error(error);
+      const loadingDiv = document.getElementById("loading");
+      //tolgo il messaggio di caricamento anche se occorre un errore
+      loadingDiv.style.display = "none";
+      alert("Errore durante la ricerca dei libri. Riprova più tardi!");
+    });
 }
 
 // Funzione per contattare l'API Open Library e recuperare la descrizione di un libro, viene aggiunto anche il div description in html
@@ -37,6 +61,7 @@ function getBookDescription(key) {
   .then(data => {
   const bookDesc = document.getElementById("book-desc");
   bookDesc.innerHTML = data.description;
+  descriptionDiv.scrollIntoView({behavior: "smooth"}); //scrolla fino alla descrizione
   })
   .catch(error => console.error(error));
   }
@@ -50,27 +75,29 @@ function getBookDescription(key) {
   
   // Funzione per gestire il click sul pulsante di ricerca e visualizzare il div 'Risultati della ricerca'
   function handleSearch() {
-  const searchDiv = document.getElementById("search-result");
   const bgImage = document.getElementsByClassName("bg-image")[0];
   const bgText = document.getElementsByClassName("bg-text")[0];
-
+  const searchDiv = document.getElementById("search-result");
+  searchDiv.innerHTML=`<div class="book-list my-3">
+    <h2 class="text-center ps-4 py-5">Search result</h2>
+    <ul id="books"></ul>
+    </div>`;
+  
+  //diminuisco la grandezza dell'immagine di sfondo per avere una maggiore chiarezza complessiva
   bgImage.style.height = "50vh";
   bgText.style.top="24%";
-  searchDiv.innerHTML=`<div class="book-list my-3">
-  <h2 class="text-center py-5">Search result</h2>
-  <ul id="books"></ul>
-</div>`;
-
-
-
+  
+  //ottengo il valore che l'utente ha scritto nel form
   const searchInput = document.getElementById("search-input");
   const category = searchInput.value.trim().toLowerCase();
   
+  //primo controllo sulla categoria immessa dall'utente
   if (category === "") {
   alert("Inserisci una categoria valida!");
   return;
   }
   
+  //chiamata alla funione searchBooks
   searchBooks(category);
   }
   
