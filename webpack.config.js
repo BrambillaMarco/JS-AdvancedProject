4e668eda308a878ed806ed82a3c6ec9fc3e5dbac
Marco Brambilla <marco.brambilla2000@gmail.com>
2023-05-19 12:14:58 +0200

    aggiunto removeListener al search btn

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './JS/script.js',
+  entry: './js/script.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
